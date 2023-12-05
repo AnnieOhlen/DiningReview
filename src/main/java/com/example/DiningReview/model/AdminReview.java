5f56bc144747385018c4e36a78574992cd5dd71a
Annie <115146344+AnnieOhlen@users.noreply.github.com>
2023-12-05 20:45:18 +0100

    Update AdminReview.java

--- src/main/java/com/example/DiningReview/model/AdminReview.java
+++ src/main/java/com/example/DiningReview/model/AdminReview.java
@@ -1,4 +1,14 @@
 package com.example.DiningReview.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdminReview {
+
+    private boolean acceptReview;
+
 }
