e711aed003d754e2c6331f1068d91203690527ae
Annie <115146344+AnnieOhlen@users.noreply.github.com>
2023-12-05 23:34:21 +0100

    Update ReviewController.java

--- src/main/java/com/example/DiningReview/repository/ReviewRepository.java
+++ src/main/java/com/example/DiningReview/repository/ReviewRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+    List<Review> findByStatus(Review.ReviewStatus reviewStatus);
 
     public List<Review> findByRestaurant(String restaurant);
 
