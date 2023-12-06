b5a294d1f50bbf5e00c5d5865ad0b5a23891c5e4
Annie <115146344+AnnieOhlen@users.noreply.github.com>
2023-12-06 23:28:28 +0100

    Update ReviewController.java

--- src/main/java/com/example/DiningReview/controller/ReviewController.java
+++ src/main/java/com/example/DiningReview/controller/ReviewController.java
@@ -2,14 +2,25 @@ package com.example.DiningReview.controller;
 
 import com.example.DiningReview.model.Review;
 import com.example.DiningReview.repository.ReviewRepository;
+<<<<<<< Updated upstream
+=======
+import org.springframework.beans.factory.annotation.Autowired;
+>>>>>>> Stashed changes
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+<<<<<<< Updated upstream
 @RequestMapping("/reviews")
 public class ReviewController {
 
+=======
+@RequestMapping("/api/reviews")
+public class ReviewController {
+
+    @Autowired
+>>>>>>> Stashed changes
     private final ReviewRepository reviewRepository;
 
     public ReviewController(ReviewRepository reviewRepo) {
@@ -21,7 +32,11 @@ public class ReviewController {
     public Review postReview(@RequestBody Review review) {
         return this.reviewRepository.save(review);
     }
+<<<<<<< Updated upstream
     
+=======
+
+>>>>>>> Stashed changes
     //Admin: get the list of all dining reviews that are pending approval.
     @GetMapping("/pending")
     public List<Review> getReviewsPending() {
@@ -31,6 +46,12 @@ public class ReviewController {
     //TODO As an admin, I want to approve or reject a given dining review.
 
 
+<<<<<<< Updated upstream
     //TODO As part of the backend process that updates a restaurant's set of scores, I want to fetch the set of all approved dining reviews belonging to this restaurant.
+=======
+
+    //TODO As part of the backend process that updates a restaurant's set of scores,
+    // I want to fetch the set of all approved dining reviews belonging to this restaurant.
+>>>>>>> Stashed changes
 
 }
